2802fe7aee77eb3784a76a594fcf83b313157bfd
amti7 <amti7@o2.pl>
2019-08-25 10:08:01 +0200

    [FMM-5] Profile UI

---
+++ Project/Source Files/Screens/Profile/ProfileCell/ProfileTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileTableViewCell.swift
+//  NetguruCollegeApp
+//
+
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet var movieTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Project/Source Files/Screens/Profile/ProfileViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewModel.swift
+//  NetguruCollegeApp
+//
+
+import UIKit
+
+class ProfileViewModel: NSObject {
+    func setupTableViewForController() {
+      
+    }
+}
+
+//extension ProfileViewModel: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "profileTableViewCell", for: indexPath) as ProfileTableViewCell
+//    }
+        
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 3
+//    }
+//}
