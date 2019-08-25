196ddb2e464c33fdc272da274decb31c0d98be70
Marcin Galus <mtgalus@gmail.com>
2019-08-25 14:16:34 +0200

    Implement search functionality and show fetched results in tableView

--- Project/Source Files/Screens/Search/SearchViewController.swift
+++ Project/Source Files/Screens/Search/SearchViewController.swift
@@ -7,16 +7,40 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    enum State {
+        case empty
+        case populated([Movie])
+    }
+
+    var state = State.empty {
+        didSet {
+            switch self.state {
+            case .populated(let movies):
+                searchViewModel.movieArray = movies
+            default:
+                break
+            }
+            DispatchQueue.main.async {
+                self.setTableViewBackground()
+                let range = Range(uncheckedBounds: (0, self.tableView.numberOfSections))
+                let indexSet = IndexSet(integersIn: range)
+                self.tableView.reloadSections(indexSet, with: .automatic)
+            }
+        }
+    }
+
     private var customView: SearchView {
         return view as! SearchView
-	}
+    }
+    private var tableView: UITableView {
+        return customView.tableView
+    }
     private var searchController = UISearchController(searchResultsController: nil)
-    private var searchScopeButtonTitles = ["Title", "Genre"]
     private let searchViewModel: SearchViewModel
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-	}
+    }
 
     init(searchViewModel: SearchViewModel) {
         self.searchViewModel = searchViewModel
@@ -28,7 +52,7 @@ class SearchViewController: UIViewController {
 
     override func loadView() {
         view = SearchView.instanceFromNib()
-	}
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,53 +62,83 @@ class SearchViewController: UIViewController {
     }
 
     private func setupTableView() {
-        customView.tableView.register(
+        tableView.register(
             UINib(nibName: "SearchViewCell", bundle: nil),
             forCellReuseIdentifier: SearchViewCell.searchViewCellReuseIdentifier
         )
-        customView.tableView.delegate = self
-        customView.tableView.dataSource = self
-        customView.tableView.rowHeight = 150
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 150
     }
 
     private func setupSearchController() {
         navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
         searchController.searchBar.placeholder = NSLocalizedString("Search", comment: "")
         navigationItem.hidesSearchBarWhenScrolling = false
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.delegate = self
-        searchController.searchBar.scopeButtonTitles = searchScopeButtonTitles
-	}
+    }
 
     private func setupSearchViewController() {
         navigationItem.title = "Search"
         definesPresentationContext = true
-	}
+    }
+
+    private func setTableViewBackground() {
+
+        func setForEmpty() {
+            //TODO: display a message as tableView background
+        }
+
+        func setForPopulated() {
+            //TODO: turn off any error backgrounds
+        }
+
+        switch state {
+        case .populated:
+            setForPopulated()
+        case .empty:
+            setForEmpty()
+        }
+    }
 }
 
 extension SearchViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        //TODO: go to detail view
     }
 }
 
 extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20 //TODO: change this when model is ready
+        return searchViewModel.movieArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: SearchViewCell.searchViewCellReuseIdentifier) as! SearchViewCell
-        cell.titleLabel.text = "Test Movie Title \(indexPath.row + 1)"	//TODO: change this when model is ready
-        cell.subtitleLabel.text = "Some sublabel"	//TODO: change this when model is ready
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: SearchViewCell.searchViewCellReuseIdentifier) as? SearchViewCell else {
+                fatalError("Can't cast tableView cell to SearchViewCell")
+        }
+        cell.titleLabel.text = searchViewModel.movieArray[indexPath.row].title
+        cell.subtitleLabel.text = String(searchViewModel.movieArray[indexPath.row].voteAverage)
         return cell
     }
 }
 
-extension SearchViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
-        print("New search scope is \(searchScopeButtonTitles[selectedScope])")	///TODO: change this when model is ready
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchQuery = searchController.searchBar.text,
+                                searchController.searchBar.text?.isEmpty == false else {
+            return
+        }
+        searchViewModel.searchMovieByTitle(searchQuery) { [weak self] (movies, error) in
+            if let movies = movies {
+                self?.state = .populated(movies)
+            } else {
+                self?.state = .empty
+            }
+        }
     }
 }
