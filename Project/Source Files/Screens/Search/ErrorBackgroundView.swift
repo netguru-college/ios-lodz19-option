5450a2e8d1c6ea958019991cd5b525918d8d52ff
Marcin Galus <mtgalus@gmail.com>
2019-08-25 15:04:59 +0200

    Add handling for empty tableView

---
+++ Project/Source Files/Screens/Search/ErrorBackgroundView.swift
@@ -0,0 +1,67 @@
+//
+//  ErrorBackgroundView.swift
+//  NetguruCollegeApp
+//
+
+import UIKit
+
+class ErrorBackgroundView: UIView {
+
+    let errorTitle = UILabel()
+    let errorDescription = UILabel()
+
+    override var frame: CGRect {
+        didSet {
+            setup()
+        }
+    }
+
+    init() {
+        super.init(frame: .zero)
+    }
+
+    required init?(coder aDecoder: NSCoder) { return nil }
+
+    func configureConstraints() {
+        guard let superview = superview else { return }
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalTo: superview.widthAnchor),
+            self.heightAnchor.constraint(equalTo: superview.heightAnchor),
+            self.centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+            self.centerYAnchor.constraint(equalTo: superview.centerYAnchor)
+            ])
+    }
+
+    private func setup() {
+        translatesAutoresizingMaskIntoConstraints = false
+        isUserInteractionEnabled = false
+        backgroundColor = .white
+        // configure errorTitle
+        addSubview(errorTitle)
+        errorTitle.text = "Oops!"
+        errorTitle.font = .systemFont(ofSize: 24)
+        errorTitle.textColor = .darkGray
+        errorTitle.textAlignment = .center
+        errorTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            errorTitle.widthAnchor.constraint(equalToConstant: 75),
+            errorTitle.heightAnchor.constraint(equalToConstant: 40),
+            errorTitle.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            errorTitle.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150)
+            ])
+        // configure errorDescription
+        addSubview(errorDescription)
+        errorDescription.font = .systemFont(ofSize: 17)
+        errorDescription.textColor = .darkText
+        errorDescription.textAlignment = .center
+        errorDescription.numberOfLines = 0
+        errorDescription.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            errorDescription.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            errorDescription.topAnchor.constraint(equalTo: errorTitle.bottomAnchor, constant: 5),
+            errorDescription.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            errorDescription.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40)
+            ])
+        errorDescription.sizeToFit()
+    }
+}
--- Project/Source Files/Screens/Search/SearchViewController.swift
+++ Project/Source Files/Screens/Search/SearchViewController.swift
@@ -9,12 +9,15 @@ class SearchViewController: UIViewController {
 
     enum State {
         case empty
+        case noSearchResults
         case populated([Movie])
     }
 
-    var state = State.empty {
+    private var state = State.empty {
         didSet {
             switch self.state {
+            case .empty:
+                searchViewModel.movieArray = []
             case .populated(let movies):
                 searchViewModel.movieArray = movies
             default:
@@ -35,6 +38,7 @@ class SearchViewController: UIViewController {
     private var tableView: UITableView {
         return customView.tableView
     }
+    private var errorBackgroundView: ErrorBackgroundView?
     private var searchController = UISearchController(searchResultsController: nil)
     private let searchViewModel: SearchViewModel
 
@@ -59,6 +63,7 @@ class SearchViewController: UIViewController {
         setupTableView()
         setupSearchViewController()
         setupSearchController()
+        setTableViewBackground()
     }
 
     private func setupTableView() {
@@ -78,6 +83,7 @@ class SearchViewController: UIViewController {
         navigationItem.hidesSearchBarWhenScrolling = false
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
     }
 
     private func setupSearchViewController() {
@@ -87,19 +93,37 @@ class SearchViewController: UIViewController {
 
     private func setTableViewBackground() {
 
+        func setErrorBackgroundView(message: String) {
+            errorBackgroundView?.removeFromSuperview()
+            errorBackgroundView = nil
+            errorBackgroundView = ErrorBackgroundView()
+            tableView.addSubview(errorBackgroundView!)
+            tableView.separatorStyle = .none
+            errorBackgroundView?.configureConstraints()
+            errorBackgroundView?.errorDescription.text = message
+        }
+
         func setForEmpty() {
-            //TODO: display a message as tableView background
+            tableView.separatorStyle = .none
+        }
+
+        func setForNoSearchResults() {
+            setErrorBackgroundView(message: "No movies found!")
         }
 
         func setForPopulated() {
-            //TODO: turn off any error backgrounds
+            tableView.subviews.forEach( { if $0 is ErrorBackgroundView { $0.removeFromSuperview() } } )
+            errorBackgroundView = nil
+            tableView.separatorStyle = .singleLine
         }
 
         switch state {
-        case .populated:
-            setForPopulated()
         case .empty:
             setForEmpty()
+        case .populated:
+            setForPopulated()
+        case .noSearchResults:
+            setForNoSearchResults()
         }
     }
 }
@@ -129,16 +153,31 @@ extension SearchViewController: UITableViewDataSource {
 
 extension SearchViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
+        if searchController.isActive == false {
+            state = .empty
+        }
         guard let searchQuery = searchController.searchBar.text,
-                                searchController.searchBar.text?.isEmpty == false else {
-            return
+            searchController.searchBar.text?.isEmpty == false else {
+                return
         }
         searchViewModel.searchMovieByTitle(searchQuery) { [weak self] (movies, error) in
             if let movies = movies {
-                self?.state = .populated(movies)
+                if movies.isEmpty == false {
+                    self?.state = .populated(movies)
+                } else {
+                    self?.state = .noSearchResults
+                }
             } else {
-                self?.state = .empty
+                self?.state = .noSearchResults
             }
+        }
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            state = .empty
         }
     }
 }
